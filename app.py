80d991ec2f5594d816700ed78208a297434f4014
Marlesli <122844039+Marlesli@users.noreply.github.com>
2025-02-13 00:39:33 -0500

    flask template API

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')  #1st route for home 
+    URL = "https://xkcd.com/1000/info.0.json"
+    req = requests.get(url = URL)
+    data = req.json()
+    return render_template('home.html', data = data)  #1st route for home 
+
+@app.route('/pastComic')
+def pastComic():
+    URL = "https://xkcd.com/1000/info.0.json"
+    req = requests.get(url = URL)
+    data = req.json()
+    return render_template('home.html', data = data) 
 
 @app.route('/album')
 def album():
